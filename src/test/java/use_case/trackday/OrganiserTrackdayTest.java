d8c89f25828d36ee37afb98dc99d419c78a7e69c
Luigi <luigi.carole@gmail.com>
2023-04-13 15:12:48 +0200

    Modifications des tests

--- src/test/java/use_case/trackday/OrganiserTrackdayTest.java
+++ src/test/java/use_case/trackday/OrganiserTrackdayTest.java
@@ -1,5 +1,12 @@
 package use_case.trackday;
 
+import model.Comissaire;
+import model.Equipement;
+import model.RolePiste;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrganiserTrackdayTest {
@@ -10,20 +17,51 @@ class OrganiserTrackdayTest {
 
     @org.junit.jupiter.api.Test
     void getNbEquipementValide() {
-        double nbEquipementValide = 90;
+        List<Equipement> equipements = new ArrayList<>(){
+            {
+                add(new Equipement(1, true));
+                add(new Equipement(2, true));
+            }
+        };
 
+        double nbEquipementValide = new OrganiserTrackday().getNbEquipementValide(equipements);
         assertTrue(nbEquipementValide >= 80, "Il y a moins de 80% d'équipements valides.");
     }
 
     @org.junit.jupiter.api.Test
     void getNbCommisaires() {
-        int nbCommisaires = 4;
-        assertTrue(nbCommisaires >= 4, "Il y a moins de 4 comissaires disponibles.");
+        List<Comissaire> comissaires = new ArrayList<>(){
+            {
+                add(new Comissaire(1, RolePiste.CHEF_PISTE));
+                add(new Comissaire(2, RolePiste.COMISSAIRE_PISTE));
+                add(new Comissaire(3, RolePiste.COMISSAIRE_PISTE));
+                add(new Comissaire(4, RolePiste.CHEF_PISTE));
+            }
+        };
+
+        assertTrue(comissaires.size() >= 4, "Il y a moins de 4 comissaires disponibles.");
     }
 
     @org.junit.jupiter.api.Test
     void checkDirecteurAvailability() {
-        boolean directeurAvailability = true;
-        assertEquals(true, directeurAvailability, "Il n'y a pas de directeur de piste disponible.");
+        List<Comissaire> comissaires = new ArrayList<>(){
+            {
+                add(new Comissaire(1, RolePiste.CHEF_PISTE));
+                add(new Comissaire(2, RolePiste.COMISSAIRE_PISTE));
+                add(new Comissaire(3, RolePiste.COMISSAIRE_PISTE));
+                add(new Comissaire(4, RolePiste.CHEF_PISTE));
+            }
+        };
+
+        boolean directeurAvailability = false;
+
+        for(Comissaire comissaire : comissaires){
+            if(comissaire.getRole() == RolePiste.CHEF_PISTE){
+                directeurAvailability = true;
+                break;
+            }
+        }
+
+        assertTrue(directeurAvailability, "Il n'y a pas de directeur de piste disponible.");
     }
 }
