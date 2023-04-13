2b1350a960457ccb237a40d073a7de6ff1e2cc59
danxomename <isacramento@myges.fr>
2023-04-13 15:16:31 +0200

    add tests organiserTrackDay

--- src/test/java/use_case/trackday/OrganiserTrackdayTest.java
+++ src/test/java/use_case/trackday/OrganiserTrackdayTest.java
@@ -3,8 +3,10 @@ package use_case.trackday;
 import model.Comissaire;
 import model.Equipement;
 import model.RolePiste;
+import model.Trackday;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -63,5 +65,69 @@ class OrganiserTrackdayTest {
         }
 
         assertTrue(directeurAvailability, "Il n'y a pas de directeur de piste disponible.");
+    }
+
+    @org.junit.jupiter.api.Test
+    void organiseTrackdayOK() {
+
+        Trackday result = new OrganiserTrackday().createTrackday(new Date(), 100, getEquipmentList(true), getVehiculeIdList(), getComissaireList(5, true));
+        assertNotNull(result);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void organiseTrackdayNotEnoughComissaire() {
+
+        Trackday result = new OrganiserTrackday().createTrackday(new Date(), 100, getEquipmentList(true), getVehiculeIdList(), getComissaireList(3, true));
+        assertNull(result);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void organiseTrackdayNoChefComissaire() {
+
+        Trackday result = new OrganiserTrackday().createTrackday(new Date(), 100, getEquipmentList(true), getVehiculeIdList(), getComissaireList(5, false));
+        assertNull(result);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void organiseTrackdayNotEnoughEquipment() {
+
+        Trackday result = new OrganiserTrackday().createTrackday(new Date(), 100, getEquipmentList(false), getVehiculeIdList(), getComissaireList(5, true));
+        assertNull(result);
+
+    }
+
+    private List<Comissaire> getComissaireList(int nbComissaires, boolean hasChef) {
+        List<Comissaire> resultList = new ArrayList<>();
+        int id = 0;
+        if(hasChef) {
+            id++;
+            resultList.add(new Comissaire(id,RolePiste.CHEF_PISTE));
+        }
+        int until = nbComissaires-id;
+        for (int i = 0; i < until; i++) {
+            id++;
+            resultList.add(new Comissaire(id,RolePiste.COMISSAIRE_PISTE));
+
+        }
+
+        return resultList;
+    }
+
+    private List<Equipement> getEquipmentList(boolean isOK) {
+        List<Equipement> resultList = new ArrayList<>();
+        int id = 0;
+        for (int i = 0; i < 10; i++) {
+            id++;
+            resultList.add(new Equipement(id, isOK));
+        }
+
+        return resultList;
+    }
+
+    private List<Integer> getVehiculeIdList(){
+        return List.of(1,2,3,4,5);
     }
 }
